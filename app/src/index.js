9399238166591ecfa6bff0b9e39523966237d9af
David Fletcher <davidfletcher@Davids-Air.lan>
2021-11-03 16:16:27 -0700

    index.js is set for the current project -- dont know if i will use all of this ish

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//redux appropriate imports
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+// import { reducer } from './reducers';
+
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore('reducer', applyMiddleware(thunk))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
