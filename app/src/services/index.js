586d35d7ce75a5dcc5190fb5a789401fac82b773
David Fletcher <davidfletcher@Davids-Air.lan>
2021-11-06 01:01:39 -0700

    looking like it working

--- app/src/services/index.js
+++ app/src/services/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import axios from 'axios';
+// import React from "react";
+// import axios from 'axios';
 
-export default function getBeers(){
-   axios.get(`https://api.punkapi.com/v2/beers`)
-      .then(res=>{
+// export default function getBeers(){
+//    axios.get(`https://api.punkapi.com/v2/beers`)
+//       .then(res=>{
          
-      })
-}
+//       })
+// }
+// this is going to end up being the way i modularize it thhe most
